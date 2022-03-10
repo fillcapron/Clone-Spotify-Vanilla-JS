9697c6f52264404a29e163516b76a5c72ccf9325
Alexander <mr.nervs@yandex.ru>
2022-03-10 21:57:21 +0300

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,90 @@
+export class SpotifyApi {
+
+    constructor(clientId, secret) {
+        this.clientId = clientId;
+        this.secret = secret;
+        this.token = this.getToken();
+    }
+
+    /**
+     * Приватный асинхронный метод получения данных из spotify api
+     * @param  {string} url адрес в виде строки
+     * @return {Object} объект ответа
+     */
+    async #getData(url) {
+        if (url) {
+            const res = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': 'Bearer ' + await this.getToken()
+                }
+            });
+
+            const json = await res.json();
+
+            return {
+                error: !!json.error,
+                status: true,
+                data: json
+            }
+
+        } else {
+            return {
+                error: true,
+                status: false,
+                data: null
+            }
+        }
+    }
+
+    /**
+     * Асинхронный метод получения токена из spotify
+     * @return {string} полученный токен в виде строки 
+     */
+    async getToken() {
+        const res = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " + btoa(this.clientId + ':' + this.secret)
+            },
+            body: 'grant_type=client_credentials'
+        })
+        const data = await res.json();
+
+        if (data.error) {
+            return 'Ошибка получения токена'
+        }
+
+        return data?.access_token;
+    }
+
+    /**
+     * Асинхронный метод получения плейлиста рекомендаций из spotify
+     * @return {Object} полученный объект рекомендаций 
+     */
+    async getFeaturedPlaylists() {
+        const res = await this.#getData('https://api.spotify.com/v1/browse/featured-playlists?country=RU');
+        return res.data;
+    }
+
+    /**
+     * Асинхронный метод получения новых релизов из spotify
+     * @return {Object} полученный объект новых релизов
+     */
+    async getNewReleasesPlaylist() {
+        const res = await this.#getData('https://api.spotify.com/v1/browse/new-releases?country=RU');
+        return res.data;
+    }
+
+    /**
+     * Асинхронный метод получения данных по поисковому запросу (плейлисты, треки)
+     * @param  {string} query
+     * @return {Object} полученный объект найденных плейлистов, треков
+     */
+    async getSearchResults(query) {
+        const res = await this.#getData(`https://api.spotify.com/v1/search?type=track,playlist&q=${query}`);
+        return res.data;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,100 @@
+import { SpotifyApi } from "./api/api.js";
+import { millisToMinutesAndSeconds } from './utils.js'
+
+const clientId = 'f03d452f6b3647c38ab26ad5f8cc8629';
+const secret = '8848ff822526499f9d0ba968f87807b3';
+
+const card = document.getElementById('card');
+const track = document.getElementById('track');
+const search = document.getElementById('search');
+const content = document.getElementById('content');
+
+const spotify = new SpotifyApi(clientId, secret);
+
+function addPlaylistElements(el, items, playlistName = '') {
+    const playlist = document.createElement('div');
+    playlist.classList.add('playlist');
+    const title = document.createElement('h2');
+    title.classList.add('title');
+    title.textContent = playlistName;
+
+
+    items.slice(0, 8).forEach((item) => {
+        const cardItem = document.createElement('div');
+        cardItem.innerHTML = card.innerHTML;
+        cardItem.classList.add('card');
+        cardItem.querySelector('.card_image').setAttribute('src', item.images[0].url);
+        cardItem.querySelector('.card_title').textContent = item.name;
+        cardItem.querySelector('.card_description').textContent = item.description && item.description.slice(0, 55);
+        playlist.append(cardItem);
+    });
+
+    if(!items.length){
+        playlist.textContent = 'Ничего не найдено';
+    }
+
+    el.append(title);
+    el.append(playlist);
+}
+
+function addTracks(el, items) {
+    const tracksElement = document.createElement('div');
+    tracksElement.classList.add('tracks');
+    const title = document.createElement('h2');
+    title.classList.add('title');
+    title.textContent = 'Найденные треки';
+
+    items.forEach((item, i) => {
+        const tracks = document.createElement('div');
+        tracks.innerHTML = track.innerHTML;
+        tracks.classList.add('tracks_item');
+        tracks.querySelector('.tracks_item_info-play-image').setAttribute('src', item.album.images[0].url);
+        tracks.querySelector('.tracks_item_info-album-name').textContent = item.name;
+        tracks.querySelector('.tracks_item_info-album-artist').textContent = item.artists[0].name;
+        tracks.querySelector('.tracks_item-ms').textContent = millisToMinutesAndSeconds(item.duration_ms);
+        tracksElement.append(tracks);
+    });
+
+    if(!items.length){
+        tracksElement.textContent = 'Ничего не найдено';
+    }
+
+    el.append(title);
+    el.append(tracksElement);
+}
+
+async function loadContent(){
+    if (await spotify.token) {
+        const featuredPlaylists = await spotify.getFeaturedPlaylists();
+        const newReleasesPlaylist = await spotify.getNewReleasesPlaylist();
+
+        addPlaylistElements(content, featuredPlaylists.playlists.items, featuredPlaylists.message);
+        addPlaylistElements(content, newReleasesPlaylist.albums.items, 'Новые релизы');
+    }
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    if (await spotify.token) {
+        const featuredPlaylists = await spotify.getFeaturedPlaylists();
+        const newReleasesPlaylist = await spotify.getNewReleasesPlaylist();
+
+        addPlaylistElements(content, featuredPlaylists.playlists.items, featuredPlaylists.message);
+        addPlaylistElements(content, newReleasesPlaylist.albums.items, 'Новые релизы');
+    }
+});
+
+
+search.addEventListener('input', async (e) => {
+    const query = e.target.value;
+    if(query.length > 3){
+        const res = await spotify.getSearchResults(query);
+        content.innerHTML = '';
+        addPlaylistElements(content, res.playlists.items, 'Плейлисты');
+        addTracks(content, res.tracks.items);
+        return
+    }
+    if(!query.length){
+        content.innerHTML = '';
+        loadContent();
+    }
+})
